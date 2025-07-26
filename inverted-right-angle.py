8690ebfaaee70c59164e574dfabba047d7321653
Lucky Jha <jhalucky61@gmail.com>
2025-07-26 14:42:27 +0530

    Inverted-right-angle-triangle

---
+++ inverted-right-angle.py
@@ -0,0 +1,13 @@
+n = int(input("Enter the length of base and perpendicular: "))
+
+for i in range(n,0,-1):
+    print("*" * i)
+
+
+# alternate method using class and object
+
+# class Solution:
+#     def printPattern(self, n):
+#         #Code here
+#         for i in range(n ,0,-1):
+#             print("* " * i)
