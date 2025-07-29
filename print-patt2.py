59e280accc1e404a6d9c306f28b6d544ec64cd30
Lucky Jha <jhalucky61@gmail.com>
2025-07-29 23:29:23 +0530

    Pattern 2

---
+++ print-patt2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def printTriangle(self, n):
+        # Code here
+        for i in range(1,n+1):
+            print("* " * i)
+        for j in range(n-1, 0, -1):
+            print("* " * j)
