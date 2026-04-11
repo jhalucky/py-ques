53813aa8ad846a1bb3dd42697c202775114e218b
Lucky Jha <jhalucky61@gmail.com>
2026-04-11 22:44:02 +0530

    ques solved

---
+++ ques9.py
@@ -0,0 +1,13 @@
+weather = input("Enter how's the weather: ")
+
+if weather == "Sunny":
+    print("Go for a walk!")
+
+elif weather == "Rainy":
+    print("Read a Book!")
+
+elif weather == "Snowy":
+    print("Build a snowman")
+
+else:
+    print("Go and Sleep!")
