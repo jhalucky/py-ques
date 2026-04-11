0b25da525b667c922d08fdc943e9df34cd4c2a06
Lucky Jha <jhalucky61@gmail.com>
2026-04-11 22:30:28 +0530

    ques solved

---
+++ ques8.py
@@ -0,0 +1,11 @@
+fruit = input("Enter a fruit name: ")
+color = input("Enter color of the fruit: ")
+
+if color == "Green":
+    print("Unripe")
+
+elif color == "Yellow":
+    print("Ripe")
+
+else:
+    print("Overripe")
