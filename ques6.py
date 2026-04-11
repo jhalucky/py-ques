71c116f8db449069b14f19b5ee54979e03bc7c3e
Lucky Jha <jhalucky61@gmail.com>
2026-04-11 21:55:33 +0530

    file added

--- ques6.py
+++ ques6.py
@@ -1,13 +1,8 @@
 age = int(input("Enter your age: "))
 
-if age < 13:
-    print("Child")
+if age < 18:
+    print("Ticket price is $8")
 
-elif age >= 13 and age <= 19:
-    print("Teenager")
-
-elif age >= 20 and age <= 59:
-    print("Adult")
 
 else:
-    print("Senior")
+    print("Ticket price is $12")
