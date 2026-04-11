f765d2fb275c90d417ca5efac50bfb4cadbc4a56
Lucky Jha <jhalucky61@gmail.com>
2026-04-11 22:27:30 +0530

    ques solved

---
+++ ques7.py
@@ -0,0 +1,19 @@
+score = int(input("Enter the score: "))
+
+if score >= 90 and score <= 100:
+    print("You got A grade.")
+
+elif score >= 80 and score <= 89:
+    print("You got B grade.")
+
+elif score >= 70 and score <= 79:
+    print("You got C grade.")
+
+elif score >= 60 and score <= 69:
+    print("You got D grade.")
+
+else:
+    print("You got E grade.")
+
+
+
