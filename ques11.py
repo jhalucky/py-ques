89aa23f61a4386fef5ffe6ef910d627d094f6aeb
Lucky Jha <jhalucky61@gmail.com>
2026-04-12 11:05:13 +0530

    ques solved

---
+++ ques11.py
@@ -0,0 +1,12 @@
+password = input("Enter the password: ")
+
+if len(password) < 6:
+    strength = "Weak"
+
+elif len(password) >= 6 and len(password) < 10:
+    strength = "Medium"
+
+else:
+    strength = "Strong"
+
+print("Password strength: ", strength)
