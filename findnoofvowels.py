a1f288ec38f2a297cd21cbc05f808335453dedd4
Lucky Jha <jhalucky61@gmail.com>
2026-04-28 15:17:09 +0530

    solved

--- findnoofvowels.py
+++ findnoofvowels.py
@@ -1,12 +1,23 @@
-def count_vowels():
-    s = input("Enter the word: ")
-    vowels = "aeiouAEIOU"
-    count = 0
+# def count_vowels():
+#     s = input("Enter the word: ")
+#     vowels = "aeiouAEIOU"
+#     count = 0
 
-    for i in s:
-        if i in vowels:
-            count += 1
-    return count
+#     for i in s:
+#         if i in vowels:
+#             count += 1
+#     return count
     
 
-print(count_vowels())
+# print(count_vowels())
+
+s = input("Enter the word: ")
+vowels = "aeiouAEIOU"
+count = 0
+
+for i in s:
+    if i in vowels:
+        count += 1
+print(count)
+    
+    
