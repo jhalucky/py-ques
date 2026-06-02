e7c8fa24bf0063c146d2ac7026381e952cf5c330
Lucky Jha <jhalucky61@gmail.com>
2026-06-02 20:22:43 +0530

    prac session

---
+++ py-practice-exercise-github -collections/ques6.py
@@ -0,0 +1,18 @@
+# swapping using third variable
+
+a = input("Enter the num: ")
+b = input("Enter second num: ")
+
+# temp = a
+# a = b
+# b = temp
+
+# print(a)
+# print(b)
+
+# swapping in oython
+
+a,b = b,a
+
+print(a)
+print(b)
