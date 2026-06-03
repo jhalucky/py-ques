03e9374bca48357a46fd55475e10b7728d81e57b
Lucky Jha <jhalucky61@gmail.com>
2026-06-03 19:44:56 +0530

    session2

--- py-practice-exercise-github -collections/ques10.py
+++ py-practice-exercise-github -collections/ques10.py
@@ -1,7 +1,14 @@
-num_list = [10, 20, 33, 46, 55]
+# filtering out nums divisible by 5
+
+# num_list = [10, 20, 33, 46, 55]
 
 
-for i in num_list:
-    if i % 5 == 0:
-        print(i)
+# for i in num_list:
+#     if i % 5 == 0:
+#         print(i)
         
+
+
+str_x = "Emma is good developer. Emma is a writer. Emma is a fucker. Emma is a good girl."
+
+print(str_x.count("Emma"))
