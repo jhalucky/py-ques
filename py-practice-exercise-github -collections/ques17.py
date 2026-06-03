0ff7aa82d76de28b677101301ac469b3a51d2fd7
Lucky Jha <jhalucky61@gmail.com>
2026-06-03 23:26:28 +0530

    session2

---
+++ py-practice-exercise-github -collections/ques17.py
@@ -0,0 +1,14 @@
+string = input("Enter the string: ")
+
+n = input("ENter term: ")
+
+result = ''
+
+for i in string:
+    if i != n:
+        result += i
+        
+
+
+print(string)
+print(result)
