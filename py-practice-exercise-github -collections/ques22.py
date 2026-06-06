12df87daef140016aca33636ce12b6b8461a695a
Lucky Jha <jhalucky61@gmail.com>
2026-06-06 09:10:40 +0530

    session4

--- py-practice-exercise-github -collections/ques22.py
+++ py-practice-exercise-github -collections/ques22.py
@@ -1,2 +1,19 @@
-for i in range(5,0,-1):
-    print("*" * i)
+# reverse pyramind
+
+# for i in range(5,0,-1):
+#     print("*" * i)
+
+
+
+
+# next question
+
+def exponent(base, exponent):
+    
+    power = base ** exponent
+    print(power)
+
+
+
+exponent(5, 5)
+exponent(2,5)
