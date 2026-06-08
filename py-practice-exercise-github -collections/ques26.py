de41730fda97cca0c1c8381de3da9dc6f31d39fa
Lucky Jha <jhalucky61@gmail.com>
2026-06-08 23:19:35 +0530

    session6

---
+++ py-practice-exercise-github -collections/ques26.py
@@ -0,0 +1,15 @@
+numbers = [12, 7, 34, 21, 5, 10, 8, 3, 19, 2]
+
+even = []
+odd = []
+
+for i in numbers:
+    if i%2 == 0:
+        even.append(i)
+    
+    else:
+        odd.append(i)
+
+
+print(even)
+print(odd)
