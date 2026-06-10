0fc1dbe7070f0fbc0b56446b4eeb07e3cdc2812c
Lucky Jha <jhalucky61@gmail.com>
2026-06-10 22:31:37 +0530

    session8

--- py-practice-exercise-github -collections/ques37.py
+++ py-practice-exercise-github -collections/ques37.py
@@ -1,12 +1,23 @@
-n = int(input("Enter starting countdown num: "))
+# this one is also right!
+# n = int(input("Enter starting countdown num: "))
 
-while n >= 0:
-    if n > 0:
-        print(n)
+# while n >= 0:
+#     if n > 0:
+#         print(n)
 
-    else:
-        print("Yeah! Let's fucking GO!")
+#     else:
+#         print("Yeah! Let's fucking GO!")
 
-    n-=1
+#     n-=1
 
 
+import time
+
+count = int(input("Enter count: "))
+
+while count > 0:
+    print(count)
+    time.sleep(1)
+    count -= 1
+
+print("Let's fucking GO!")
