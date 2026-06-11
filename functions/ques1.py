6d3872e681b0881c75c5c17a0bd24e5d2b5357f0
Lucky Jha <jhalucky61@gmail.com>
2026-06-11 23:17:59 +0530

    doc fetching

--- functions/ques1.py
+++ functions/ques1.py
@@ -16,3 +16,10 @@ print(is_even('hello'))
 n = int(input("Enter any number: "))
 for i in range(1,n):
     print(i, is_even(i))
+
+
+# how to access doc string of any function
+
+print(is_even.__doc__)
+
+# using __doc__ we can access any function's documentation
