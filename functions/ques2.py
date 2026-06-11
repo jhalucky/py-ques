e6ec8c8fff46e804d22c5add700f2198f926a5bc
Lucky Jha <jhalucky61@gmail.com>
2026-06-11 23:16:00 +0530

    LOL

--- functions/ques2.py
+++ functions/ques2.py
@@ -1,11 +1,13 @@
 # *args
 
+
 def multiply(*args):
 
     product = 1
 
     for i in args:
         product = product * i
+    print(args)
     return product
 
 print(multiply(1,2,3,4,5,6,7,8,9,10,12)) 
