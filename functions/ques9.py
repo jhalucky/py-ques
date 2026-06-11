69167c74d6aa09ec35268b72e86dd63899f46e46
Lucky Jha <jhalucky61@gmail.com>
2026-06-11 23:46:30 +0530

    ques9

---
+++ functions/ques9.py
@@ -0,0 +1,12 @@
+def generate_list():
+
+    # even_nums = []
+
+    # for i in range(4,30):
+    #     if i % 2 == 0:
+    #         even_nums.append(i)
+
+    # return even_nums
+
+
+print(generate_list())
