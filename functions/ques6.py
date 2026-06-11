5da24e0266c959facea06feac95427d854da3d9f
Lucky Jha <jhalucky61@gmail.com>
2026-06-11 23:30:41 +0530

    update in ques6

--- functions/ques6.py
+++ functions/ques6.py
@@ -1,8 +1,8 @@
-def show_employee(name, salary=9000):
+def show_employee(name, salary=9000,role="regular staff"):
 
-    print(f"name = {name}, salary = {salary}")
+    print(f"name = {name}, salary = {salary}, role = {role}")
 
 
-show_employee("Ben", 50000)
-show_employee("Lucky", 100000)
+show_employee("Ben", 50000, "Web Developer")
+show_employee("Lucky", 100000, "Data Engineer")
 show_employee("ben")
