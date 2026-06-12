dca0762dd7ad86f3154b8315ffc6bae8501125ef
Lucky Jha <jhalucky61@gmail.com>
2026-06-12 23:51:12 +0530

    list-ques4

--- lists/ques3.py
+++ lists/ques3.py
@@ -1,6 +1,7 @@
 sum = 0
 
 numbers = [10, 20, 30, 40, 50]
+# here we could have use sum() function but just to practice loops i did this ow it depends on you how you want to do
 for i in numbers:
     sum+=i
 
---
+++ lists/ques4.py
@@ -0,0 +1,4 @@
+data = [45, 12, 67, 89, 2]
+
+print(f"Maximum: {max(data)}")
+print(f"Minimum: {min(data)}")
