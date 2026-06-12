6699cc7a734f1569e6d845ce7a349cc41006c9ef
Lucky Jha <jhalucky61@gmail.com>
2026-06-12 23:18:00 +0530

    ques11

---
+++ functions/ques11.py
@@ -0,0 +1,6 @@
+def describe_pet(animal_type, pet_name):
+    print(f"I have a {animal_type}.\nIt's name is {pet_name}. ")
+
+
+describe_pet("hamster", "Harry")
+describe_pet(pet_name="Sweety", animal_type="Cat")
