55c943eb31f2aaeffae653d0866ffee1b530d531
Lucky Jha <jhalucky61@gmail.com>
2026-06-12 23:36:47 +0530

    list-ques1

---
+++ lists/ques1.py
@@ -0,0 +1,8 @@
+numbers = [10, 20, 30, 40, 50]
+
+is_empty = len(numbers) == 0
+    
+
+print(f"Third Element: {numbers[2]}")
+print(f"Length of list: {len(numbers)}")
+print(f"Is this list empty: {is_empty}")
