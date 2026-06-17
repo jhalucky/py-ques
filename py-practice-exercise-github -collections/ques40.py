222e08ca66de1c38f7746bfdbef5fed7eb961c24
Lucky Jha <jhalucky61@gmail.com>
2026-06-17 23:56:48 +0530

    ques40

---
+++ py-practice-exercise-github -collections/ques40.py
@@ -0,0 +1,7 @@
+def display(**kwargs):
+
+    for (key,value) in kwargs.items():
+        print (key, "->" ,value)
+    
+
+display(India="Delhi", Srilanka="Colombo", Nepal="Kathmandu", Pakistan="ISlamabad")
