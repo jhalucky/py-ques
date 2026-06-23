1b781cb94f34f800eb44a6af8fa8587fd69e91e9
Lucky Jha <jhalucky61@gmail.com>
2026-06-23 10:28:17 +0530

    oops-fraction-dt-sub

--- OOPs/fraction-dt.py
+++ OOPs/fraction-dt.py
@@ -10,7 +10,12 @@ class Fraction:
         new_num = self.numerator*other.denominator + other.numerator*self.denominator
         new_den = self.denominator*other.denominator
         return '{}/{}'.format(new_num, new_den)
-
+    
+    def __sub__(self, other):
+        new_num = self.numerator*other.denominator - other.numerator*self.denominator
+        new_den = self.denominator*other.denominator
+        return '{}/{}'.format(new_num, new_den) 
+    
     def __str__(self):
         return '{}/{}'.format(self.numerator,self.denominator) # "{self.numerator}/{self.denominator} will also work but it will not give the output in the form of fraction, it will give the output in the form of string"
     
@@ -27,3 +32,4 @@ print(type(obj1))
 obj2 = Fraction(3,9)
 print(obj1, obj2)
 print(obj1 + obj2)
+print(obj1 - obj2)
