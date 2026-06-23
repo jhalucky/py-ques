64538a7c013af5315bc485e0f4fe80615e0a61f7
Lucky Jha <jhalucky61@gmail.com>
2026-06-23 20:17:54 +0530

    2d-coordinate

---
+++ OOPs/2d-coordinate.py
@@ -0,0 +1,46 @@
+class Coordinate:
+    
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        # self.origin = ''
+    
+    def __str__(self):
+        return f"({self.x},{self.y})"
+    
+    
+    def points(self,other):
+        x1 = self.x
+        y1 = self.y
+        x2 = other.x
+        y2 = other.y
+        return f"coord1: ({self.x},{self.y}), coord2: ({other.x}, {other.y})"
+    
+    def distance(self, other):
+        x1 = self.x
+        x2 = other.x
+        y1 = self.y
+        y2 = other.y
+        distance = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+        return distance
+    
+    def distance_from_origin(self):
+        x1 = 0
+        y1 = 0
+        x2 = self.x
+        y2 = self.y
+        distance = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+        return distance
+        
+
+
+
+
+
+obj1 = Coordinate(1,2)
+obj2 = Coordinate(0,0)
+print(type(obj1))
+distance1 = Coordinate.distance(obj1, obj2)
+distance2 = Coordinate.distance_from_origin(obj1)
+print(obj1, obj2)
+print(distance1, distance2)
--- OOPs/fraction-dt.py
+++ OOPs/fraction-dt.py
@@ -3,7 +3,7 @@ class Fraction:
     def __init__(self,num,den): #parameterized constructor
         self.numerator = num
         self.denominator = den
-        
+        self.convert_to_decimal()
     
 
     def __add__(self, other):
@@ -28,6 +28,13 @@ class Fraction:
         new_den = self.denominator*other.numerator
         return '{}/{}'.format(new_num, new_den)
     
+
+    
+    def convert_to_decimal(self):
+        return self.numerator/self.denominator
+    
+
+
     def __str__(self):
         return '{}/{}'.format(self.numerator,self.denominator) # "{self.numerator}/{self.denominator} will also work but it will not give the output in the form of fraction, it will give the output in the form of string"
     
@@ -39,9 +46,10 @@ class Fraction:
 
 
 
-obj1 = Fraction(2,9)
+obj1 = Fraction(2,1)
 print(type(obj1))
-obj2 = Fraction(3,9)
+obj2 = Fraction(3,1)
+print(obj1.convert_to_decimal())
 print(obj1, obj2)
 print(obj1 + obj2)
 print(obj1 - obj2)
