4f789c86c3fa62bad2299c49043b743efcbbeb2b
Lucky Jha <jhalucky61@gmail.com>
2026-06-27 18:41:50 +0530

    Inheritance

--- OOPs/inheritance.py
+++ OOPs/inheritance.py
@@ -1,24 +1,50 @@
-class User:
+# class User:
 
-    def __init__(self):
-        self.name = 'Lucky'
-    def login(self):
-        print('Login here')
+#     def __init__(self):
+#         self.name = 'Lucky'
+#     def login(self):
+#         print('Login here')
 
 
-class Student(User):
+# class Student(User):
 
-    # def __init__(self, roll_no):
-    #     self.roll_no = roll_no
+#     # def __init__(self, roll_no):
+#     #     self.roll_no = roll_no
 
-    def enroll(self):
-        print('Enroll for courses')
+#     def enroll(self):
+#         print('Enroll for courses')
 
-# name = 'Lucky'
-u = User()
-s = Student()
+# # name = 'Lucky'
+# u = User()
+# s = Student()
     
-print(s.name)
-# print(s.roll_no)
-s.login()
-s.enroll()
+# print(s.name)
+# # print(s.roll_no)
+# s.login()
+# s.enroll()
+
+
+class Parent:
+
+    def __init__(self, num):
+        self.__num = num
+
+    def get_num(self):
+        return self.__num
+    
+
+class Child(Parent):
+
+    def __init__(self, num, val):
+        super().__init__(num)
+        self.__val = val
+
+    def get_val(self):
+        return self.__val
+
+
+c = Child(100,200)
+print(c.get_num())
+print(c.get_val())
+
+
