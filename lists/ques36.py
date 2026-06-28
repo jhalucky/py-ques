1b76059f5fbaa2fb84a032b5069f1078bc98723e
Lucky Jha <jhalucky61@gmail.com>
2026-06-28 21:02:46 +0530

    ques36

--- lists/ques36.py
+++ lists/ques36.py
@@ -1,4 +1,12 @@
 twod_list = [[1, 2, 3], [4, 5], [6, 7, 8, 9]]
 
-flat_list = [item for sublist in twod_list for item in sublist]
+# flat_list = [item for sublist in twod_list for item in sublist]
+
+
+flat_list = []
+
+for sublist in twod_list:
+    flat_list.extend(sublist)
+
+    
 print(flat_list)
