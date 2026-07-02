815c9cb52f66ef790fbeda529d99f398bc2a0063
Lucky Jha <jhalucky61@gmail.com>
2026-07-02 09:34:27 +0530

    encap-ques3

--- OOPs/encapsulation/ques3.py
+++ OOPs/encapsulation/ques3.py
@@ -1,16 +1,53 @@
 # Statement: Write a program that uses datetime module within a class. Enter manufacturing date and expiry date of the product. The program must display the years, months and days that are left for expiry.
-from datetime import date
+from datetime import datetime
 
 class Product:
 
-    def __init__(self, name, manufacturing_date, expiry_date):
-        self.name = name
-        self.manufacturing_date = manufacturing_date
-        self.expiry_date = expiry_date
+    def __init__(self):
+        self.name = input("What's the product? : ")
+        self.manufacturing_date = input("Enter mfg date(DD-MM-YYYY): ")
+        self.expiry_date = input("Enter expiry date(DD-MM-YYYY): ")
+
+        self.manufacturing_date = datetime.strptime(self.manufacturing_date, "%d-%m-%Y")
+        self.expiry_date = datetime.strptime(self.expiry_date, "%d-%m-%Y")
+
+    def expiry_left(self):
+
+        today = datetime.today()
+        # print(today)
+
+        if self.expiry_date < today:
+            print("The product has already expired.")
+
+        else:
+            diff = self.expiry_date - today
+            days = diff.days
+
+            years = days // 365
+            days = days % 365
+            months = days // 30
+            days = days % 30
+
+            print("Time left for expiry:")
+            print("Years:", years)
+            print("Months:", months)
+            print("Days:", days)
 
 
-    def remaining_time(self):
 
-        today = date.today()
 
-        
+
+
+p = Product()
+p.expiry_left()
+p.expiry_left()
+
+
+
+
+
+
+    # def remaining_time(self):
+
+    #     today = date.today()
+
